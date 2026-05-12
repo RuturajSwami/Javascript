28fa403a86aeb3b4b3944537e450966b009b0a4e
Ruturaj Swami <123ruturajswami@gmail.com>
2026-05-12 23:07:41 +0530

    Add files via upload

---
+++ Frist.js
@@ -0,0 +1,22 @@
+console.log("Its Day1");
+
+console.log("Hello World ");
+
+let name = "Ruturaj";
+let surname = "Swami";
+let Fullname = name;
+Fullname = Fullname + " " + surname;
+
+console.log(Fullname);
+
+// Object in JS
+const neet = {
+    Title: "Neet exam has been conducted",// string
+    month: "May",                         // string
+    date :  3 ,                           // num
+    isGiven : true,                       // boolen
+    Marks : 0,                            // null, undefined
+    Note : "Exam has been cancalled now students has to give exam again (gg for students)",
+};
+
+console.log(neet);
